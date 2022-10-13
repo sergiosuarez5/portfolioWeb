933fcbe12064390f9a501e17d9430d7fe7edc930
“sergio <sergiowork98@gmail.com>
2022-10-13 19:27:26 -0300

    inicio mi portfolio web

---
+++ README.md
@@ -0,0 +1,1 @@
+# Portfolio responsive completo Sergio Suarez
---
+++ assets/js/typed.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".autoType", {
+    strings: ["¡Hola! <br> Soy Sergio <br> Frontend Developer"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true
+})
